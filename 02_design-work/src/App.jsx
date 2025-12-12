e8563f06bf0c3ced01d640641c7921b519dc0e33
nihal <nihalsheikh585@gmail.com>
2025-12-12 08:16:00 +0530

    ADDED: Tailwind to react project

---
+++ 02_design-work/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1 className="p-8 font-xl font-bold border-4 border-red-800 rounded-2xl cursor-pointer border-xl bg-red-900 text-zinc-200 hover:bg-green-500 hover:border-green-900 hover:text-zinc-950">
+				Adding Tailwind CSS to React app
+			</h1>
+		</div>
+	);
+}
+
+export default App;
